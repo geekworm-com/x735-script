87419d2da951ae2f99b933b563a7d14f8fe7288f
Molham Al Nasr <dr.molham@gmail.com>
2023-08-02 13:15:51 +0200

    improve create-version.sh

--- .github/workflows/bin/create-version.sh
+++ .github/workflows/bin/create-version.sh
@@ -6,14 +6,11 @@ readonly PACKAGE_PATH="$1"
 readonly PACKAGE_VERSION="$2"
 declare -a ARCH_LIST=("arm64" "armhf")
 
-mkdir versions
+# Create the "versions" directory if it doesn't exist
+mkdir -p versions
 
 for arch_type in "${ARCH_LIST[@]}"; do
-    # run docker image and build the package in the mounted dirctory
-    sudo docker run --rm -v $PACKAGE_PATH:/pkg multiarch/debian-debootstrap:${arch_type}-bullseye \
-    dpkg-deb --build /pkg/x735-script-pkg versions/x735-script_${PACKAGE_VERSION}_${arch_type}.deb
+    # Run docker image and build the package in the mounted directory
+    docker run --rm -v "$PACKAGE_PATH":/pkg multiarch/debian-debootstrap:"${arch_type}"-bullseye \
+    dpkg-deb --build /pkg/x735-script-pkg "versions/x735-script_${PACKAGE_VERSION}_${arch_type}.deb"
 done
-
-
-
-
