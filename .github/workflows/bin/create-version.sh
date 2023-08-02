94d4f684fc65a41b1ae58bacb7086ad8a6161b8a
Molham Al Nasr <dr.molham@gmail.com>
2023-08-02 17:18:01 +0200

    fix create-version script

--- .github/workflows/bin/create-version.sh
+++ .github/workflows/bin/create-version.sh
@@ -6,15 +6,11 @@ readonly PACKAGE_PATH="$1"
 readonly PACKAGE_VERSION="$2"
 declare -a ARCH_LIST=("arm64" "armhf")
 
-ls -la "${PACKAGE_PATH}"
-ls -la "${PWD}"
 # Create the "versions" directory if it doesn't exist
 mkdir -p ${PACKAGE_PATH}/versions
-ls -la "${PACKAGE_PATH}"
-ls -la "${PWD}"
 
 for arch_type in "${ARCH_LIST[@]}"; do
     # Run docker image and build the package in the mounted directory
     docker run --rm -v "$PACKAGE_PATH":/pkg multiarch/debian-debootstrap:"${arch_type}"-bullseye \
-    dpkg-deb --build /pkg/x735-script-pkg "versions/x735-script_${PACKAGE_VERSION}_${arch_type}.deb"
+    dpkg-deb --build /pkg/x735-script-pkg "/pkg/versions/x735-script_${PACKAGE_VERSION}_${arch_type}.deb"
 done
