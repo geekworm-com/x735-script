af4d7c9cd567d287e4f18cad731e723d2e2334ff
Molham Al Nasr <dr.molham@gmail.com>
2023-08-08 11:36:02 +0200

    update release note automaticallly for new releases

--- .github/workflows/bin/create_release_note.sh
+++ .github/workflows/bin/create_release_note.sh
@@ -9,7 +9,7 @@ terminate() {
     exit "${err_code}"
 }
 
-if [[ $# -ne 4 ]]; then
+if [[ $# -ne 6 ]]; then
     terminate "Usage: $0 TEMPLATE_FILE OUTPUT_FILE TAG_NAME RELEASE_PUBLICH_DATE GITHUB_REPO" "${INVALID_CLI_ARG_COUNT}"
 fi
 
@@ -17,8 +17,11 @@ readonly TEMPLATE_FILE_PATH=$1
 readonly TAG_NAME=$2
 readonly RELEASE_PUBLICH_DATE=$3
 readonly GITHUB_REPO=$4
+readonly TOKEN=$5
+readonly RELEASE_ID=$6
+readonly CHANGELOG_MSG=$7
 
-awk -v tag_name="$TAG_NAME" -v release_date="$RELEASE_PUBLICH_DATE" -v repo="$GITHUB_REPO" '
+release_note_text=$(awk -v tag_name="$TAG_NAME" -v release_date="$RELEASE_PUBLICH_DATE" -v repo="$GITHUB_REPO" -v msg="$CHANGELOG_MSG" '
     BEGIN {
         placeholder = 0
     }
@@ -46,4 +49,12 @@ awk -v tag_name="$TAG_NAME" -v release_date="$RELEASE_PUBLICH_DATE" -v repo="$GI
             printf "\n"
         }
     }
-' "$TEMPLATE_FILE_PATH"
+' "$TEMPLATE_FILE_PATH")
+
+curl -L \
+  -X PATCH \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/${GITHUB_REPO}/releases/${RELEASE_ID}" \
+  -d "{\"body\":\"${release_note_text}\""
