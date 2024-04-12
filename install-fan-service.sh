618a1bd241e79773508b290d40fe98e5780fa909
Geekworm.com <46470628+geekworm-com@users.noreply.github.com>
2024-04-12 11:51:05 +0800

    Update install-fan-service.sh

--- install-fan-service.sh
+++ install-fan-service.sh
@@ -4,7 +4,7 @@ echo "Start installing fan service..."
 
 #Change the PWM_CHIP_PATH to /sys/class/pwm/pwmchip2 on line 7 of x735fan.sh if you use it on Raspberry Pi 5 hardware.
 sudo cp -f ./x735-fan.sh                /usr/local/bin/
-sudp cp -f ./pwm_fan_control.py         /usr/local/bin/
+sudo cp -f ./pwm_fan_control.py         /usr/local/bin/
 sudo cp -f ./x735-fan.service           /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable x735-fan
