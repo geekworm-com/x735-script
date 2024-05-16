ba68b48d87308d875e6f80d481edd9b0661ea0ef
huangyp <harry@geekworm.com>
2024-05-16 10:03:04 +0800

    update

--- install-sss.sh
+++ install-sss.sh
@@ -5,8 +5,8 @@ echo "Start installing the soft shutdown script (sss)..."
 
 sudo cp -f ./xSoft.sh                /usr/local/bin/
 
-echo "Create a alias 'x735off' command to execute the software shutdown"
-echo 'alias x735off="sudo /usr/local/bin/xSoft.sh 0 20"' >> ~/.bashrc
-source ~/.bashrc
+# echo "Create a alias 'x735off' command to execute the software shutdown"
+# echo 'alias x735off="sudo /usr/local/bin/xSoft.sh 0 20"' >> ~/.bashrc
+# source ~/.bashrc
 
 echo "Soft shutdown script (sss) installed"
