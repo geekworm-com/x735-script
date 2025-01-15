8018ed2026335048a26d638804d655b2907e4ebf
huangyp <harry@geekworm.com>
2025-01-15 17:02:02 +0800

    update

--- xSoft.sh
+++ xSoft.sh
@@ -2,22 +2,22 @@
 
 #  Use gpiod instead of obsolete interface, and suuports ubuntu 23.04 also
 
-# 检查是否提供了足够的命令行参数
+# Check if enough command line arguments were provided
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <pwm_chip> <button_pin>" >&2
+    echo "Usage: $0 <gpio_chip> <button_pin>" >&2
     exit 1
 fi
 
-PWMCHIP=$1
+GPIOCHIP=$1
 BUTTON=$2
 
 SLEEP=4
 
-# 检查传入的参数是否为整数
+# Checks if the passed parameter is an integer
 
 re='^[0-9\.]+$'
-if ! [[ $PWMCHIP =~ $re ]] ; then
-   echo "error: pwm_chip is not a number" >&2; exit 1
+if ! [[ $GPIOCHIP =~ $re ]] ; then
+   echo "error: gpio_chip is not a number" >&2; exit 1
 fi
 
 if ! [[ $BUTTON =~ $re ]] ; then
@@ -26,4 +26,10 @@ fi
 
 echo "Your device will be shutting down in $SLEEP seconds..."
 
-gpioset --mode=time -s $SLEEP $PWMCHIP $BUTTON=1
+gpioset $GPIOCHIP $BUTTON=1
+
+sleep $SLEEP
+
+# Restore GPIO
+# This step is necessary, otherwise you will have to press the onboard button twice to turn on the device, and the same applies to the AUTO ON function.
+gpioset $GPIOCHIP $BUTTON=0
